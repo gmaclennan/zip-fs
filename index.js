bbfc2651f05ce5b066360713932b69c8221e95f1
Gregor MacLennan <gmaclennan@digital-democracy.org>
2019-11-06 14:05:06 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,104 @@
+const yauzl = require('yauzl')
+const EventEmitter = require('events')
+const duplexify = require('duplexify')
+const concat = require('concat-stream')
+const once = require('once')
+
+class Raz extends EventEmitter {
+  constructor (path, cb) {
+    super()
+    this.path = path
+    this._state = 'opening'
+    yauzl.open(this.path, { autoClose: false }, (err, zipfile) => {
+      if (err) {
+        this._state = 'error'
+        this._error = err
+        this.emit('error', err)
+        return cb(err)
+      }
+      this._state = 'reading'
+      this._zipfile = zipfile
+      this._entries = {}
+      zipfile.on('entry', entry => {
+        // Ignore directory entries
+        if (/\/$/.test(entry.fileName)) return
+        this._entries[entry.fileName] = entry
+      })
+      zipfile.on('error', err => {
+        this._state = 'error'
+        this._error = err
+        zipfile.close()
+        this.emit('error', err)
+        cb(err)
+      })
+      zipfile.on('end', () => {
+        this._state = 'ready'
+        this.emit('ready')
+        cb()
+      })
+    })
+  }
+
+  _onReady (fn) {
+    switch (this._state) {
+      case 'error':
+        process.nextTick(fn, this._error || new Error('Unknown error'))
+        break
+      case 'opening':
+      case 'reading':
+        this.once('ready', onready)
+        this.once('error', onerror)
+        break
+      case 'ready':
+        process.nextTick(fn)
+        break
+      case 'closed':
+        process.nextTick(fn, new Error('File has already been closed'))
+        break
+    }
+
+    function onready () {
+      this.removeListener('error', onerror)
+      fn()
+    }
+
+    function onerror (err) {
+      this.removeListener('ready', onready)
+      fn(err)
+    }
+  }
+
+  createReadStream (fileName, opts) {
+    opts = opts || {}
+    if (typeof opts === 'string') opts = { encoding: opts }
+    const dup = duplexify()
+    dup.setWritable(null)
+    this._onReady(err => {
+      if (err) return dup.destroy(err)
+      const entry = this._entries[fileName]
+      if (!entry) return dup.destroy(new Error('NotFound: ' + fileName))
+      this._zipfile.openReadStream(entry, (err, rs) => {
+        if (err) return dup.destroy(err)
+        if (opts.encoding) dup.setEncoding(opts.encoding)
+        dup.setReadable(rs)
+      })
+    })
+    return dup
+  }
+
+  readFile (fileName, opts, cb) {
+    if (typeof opts === 'function') cb = opts
+    cb = once(cb)
+    const rs = this.createReadStream(fileName, opts)
+    rs.on('error', cb)
+    rs.pipe(concat(data => cb(null, data)))
+  }
+
+  close () {
+    this._state = 'closed'
+    if (!this._zipfile) return
+    this._zipfile.close()
+  }
+}
+
+module.exports = Raz
---
+++ test/test.js
@@ -0,0 +1,126 @@
+const test = require('tape')
+const fs = require('fs')
+const path = require('path')
+const concat = require('concat-stream')
+const Raz = require('../')
+
+test('Read tests for good zip files', t => {
+  const files = fs.readdirSync(path.join(__dirname, 'success'))
+  const zips = files.filter(filename => /\.zip$/.test(filename))
+
+  zips.forEach(filename => {
+    t.test('-> Checking read of ' + filename, t => {
+      const zipFilename = path.join(__dirname, 'success', filename)
+      const expectedFilename = path.join(
+        zipFilename.replace(/\.zip$/, ''),
+        'a.txt'
+      )
+      const zip = new Raz(zipFilename, err =>
+        t.error(err, 'No error when opening zipfile')
+      )
+
+      t.test('  -> Read stream as buffer', t => {
+        t.plan(1)
+        const rs = zip.createReadStream('a.txt')
+        rs.on('error', t.fail)
+        const expectedBuf = fs.readFileSync(expectedFilename)
+        rs.pipe(
+          concat(data => t.deepEqual(data, expectedBuf, 'File contents match'))
+        )
+      })
+
+      t.test('  -> Read stream as utf8 encoding', t => {
+        t.plan(1)
+        const rs = zip.createReadStream('a.txt', { encoding: 'utf8' })
+        rs.on('error', t.fail)
+        const expectedBuf = fs.readFileSync(expectedFilename, 'utf8')
+        rs.pipe(
+          concat(data => t.deepEqual(data, expectedBuf, 'File contents match'))
+        )
+      })
+
+      t.test('  -> Read stream as utf8 encoding (opts as string)', t => {
+        t.plan(1)
+        const rs = zip.createReadStream('a.txt', 'utf8')
+        rs.on('error', t.fail)
+        const expectedBuf = fs.readFileSync(expectedFilename, 'utf8')
+        rs.pipe(
+          concat(data => t.deepEqual(data, expectedBuf, 'File contents match'))
+        )
+      })
+
+      t.test('  -> Read as buffer', t => {
+        t.plan(2)
+        const expectedBuf = fs.readFileSync(expectedFilename)
+        zip.readFile('a.txt', (err, data) => {
+          t.error(err, 'Error is null')
+          t.deepEqual(data, expectedBuf, 'File contents match')
+        })
+      })
+
+      t.test('  -> Read as utf8', t => {
+        t.plan(2)
+        const expectedBuf = fs.readFileSync(expectedFilename, 'utf8')
+        zip.readFile('a.txt', { encoding: 'utf8' }, (err, data) => {
+          t.error(err, 'Error is null')
+          t.equal(data, expectedBuf, 'File contents match')
+        })
+      })
+
+      t.test('  -> Read as utf8 (opts as string)', t => {
+        t.plan(2)
+        const expectedBuf = fs.readFileSync(expectedFilename, 'utf8')
+        zip.readFile('a.txt', 'utf8', (err, data) => {
+          t.error(err, 'Error is null')
+          t.equal(data, expectedBuf, 'File contents match')
+        })
+      })
+
+      t.test('  -> Emits error reading missing file', t => {
+        t.plan(3)
+        const rs = zip.createReadStream('c.txt', 'utf8')
+        rs.on('error', err => {
+          t.ok(err instanceof Error, 'Throws an error')
+          t.ok(
+            /NotFound/.test(err.message),
+            'Error message includes "NotFound"'
+          )
+        })
+        rs.on('close', () => t.pass('Emits close event'))
+      })
+
+      t.test('cleanup', t => {
+        zip.close()
+        t.end()
+      })
+
+      t.end()
+    })
+  })
+})
+
+test('Bad zipfile tests', t => {
+  const badZips = fs
+    .readdirSync(path.join(__dirname, 'failure'))
+    .concat('does_not_exist.zip')
+
+  badZips.forEach(filename => {
+    const filepath = path.join(__dirname, 'failure', filename)
+    t.test('Testing bad zip: ' + filename, t => {
+      t.plan(3)
+      const zip = new Raz(filepath, err => {
+        t.ok(err instanceof Error, 'Callback called with error')
+      })
+      const rs = zip.createReadStream('a.txt')
+      rs.on('error', err => {
+        t.ok(err instanceof Error, 'Readstream emits error')
+      })
+      rs.on('data', data => t.fail('should not emit data events'))
+      zip.readFile('a.txt', err => {
+        t.ok(err instanceof Error, 'Callback called with error')
+      })
+    })
+  })
+})
+
+// TODO: Tests for closind and cleaning up file descriptors
